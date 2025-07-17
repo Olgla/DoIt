8354e06ffc4545e859e92fff083ddf152a538335
Olgla <elenagladenko@gmail.com>
2025-07-16 22:14:31 -0400

    feat: add task state value message

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 {
@@ -9,11 +10,17 @@ import { TodoForm } from './TodoForm';
 }
 
 function App() {
+  {
+    /*const [taskCurrentState, setNewStateFunction] = useState('new state value'); //my destructured variables names*/
+  }
+
+  const [initialValue, setNewTodo] = useState('new state value');
   return (
     <>
       <h1>Todo List</h1>
       <div>
         <TodoForm />
+        <p>{initialValue}</p>
         <TodoList />
       </div>
     </>
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -2,11 +2,12 @@ function TodoListItem(props) {
   return <li>{props.title}</li>;
 }
 
+export default TodoListItem;
+
 {
   /*DESTRUCTURED:
     TodoListItem({ title }) {
         return <li>{title}</li>;
-}*/
+}        
+*/
 }
-
-export default TodoListItem;
