39050edb74f65b59c885687b3f021f540a7ec834
Olgla <elenagladenko@gmail.com>
2025-07-25 16:42:00 -0400

    Adding an Event Handler to the Form

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,8 +1,21 @@
-export function TodoForm(onAddTodo) {
+import { useRef } from 'react';
+
+export function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+
+    onAddTodo(title);
+    event.target.title.value = '';
+    todoTitleInput.current.focus();
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
       <button>Add Todo</button>
     </form>
   );
